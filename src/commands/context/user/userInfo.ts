fc3831f1409e7f4fa05688ac9e2b3a0e2f5f32e4
ExampleWasTaken <58574351+ExampleWasTaken@users.noreply.github.com>
2024-10-26 16:48:52 +0200

    fix user info context menu

--- src/commands/context/user/userInfo.ts
+++ src/commands/context/user/userInfo.ts
@@ -1,42 +1,14 @@
-import { ApplicationCommandType } from 'discord.js';
+import { ApplicationCommandType, Colors, GuildMember, RoleMention } from 'discord.js';
 import moment from 'moment/moment';
 import { constantsConfig, contextMenuCommand, contextMenuCommandStructure, makeEmbed } from '../../../lib';
 
-const data = contextMenuCommandStructure({
-  name: 'User Info',
-  type: ApplicationCommandType.User,
-  default_member_permissions: constantsConfig.commandPermission.MANAGE_SERVER,
-  dm_permission: false,
-});
-
-const beautifiedStatus: { [key: string]: string } = {
-  ONLINE: 'Online',
-  IDLE: 'Idle',
-  DND: 'Do Not Disturb',
-  OFFLINE: 'Offline',
-};
-
-export default contextMenuCommand(data, async ({ interaction }) => {
-  const targetMember = interaction.guild?.members.cache.get(interaction.targetId)!;
-
-  const filteredRoles = targetMember.roles.cache.filter((role) => role.id !== interaction.guild.id);
-  const listedRoles = filteredRoles.sort((a, b) => b.position - a.position).map((role) => role.toString());
-
-  const onlineStatus = beautifiedStatus[targetMember.presence?.status?.toUpperCase() ?? 'OFFLINE'];
-
-  let status;
-  if (targetMember.presence == null) {
-    status = 'Offline';
-  } else {
-    status = onlineStatus;
-  }
-
-  const whoisEmbed = makeEmbed({
+const whoisEmbed = (targetMember: GuildMember, status: string, listedRoles: RoleMention[]) =>
+  makeEmbed({
     author: {
       name: targetMember.user.username,
       iconURL: targetMember.user.avatarURL()!,
     },
-    description: `${targetMember}`,
+    description: `${targetMember.toString()}`,
     thumbnail: { url: targetMember.user.avatarURL()! },
     fields: [
       {
@@ -76,5 +48,43 @@ export default contextMenuCommand(data, async ({ interaction }) => {
     footer: { text: `User ID: ${targetMember.id}` },
   });
 
-  return interaction.reply({ embeds: [whoisEmbed] });
+const memberNotFoundEmbed = makeEmbed({
+  description: 'Could not find the requested member.',
+  color: Colors.Red,
+});
+
+const beautifiedStatus: { [key: string]: string } = {
+  ONLINE: 'Online',
+  IDLE: 'Idle',
+  DND: 'Do Not Disturb',
+  OFFLINE: 'Offline',
+};
+
+const data = contextMenuCommandStructure({
+  name: 'User Info',
+  type: ApplicationCommandType.User,
+  default_member_permissions: constantsConfig.commandPermission.MANAGE_SERVER,
+  dm_permission: false,
+});
+
+export default contextMenuCommand(data, async ({ interaction }) => {
+  const targetMember = interaction.guild.members.cache.get(interaction.targetId);
+
+  if (!targetMember) {
+    return interaction.reply({ embeds: [memberNotFoundEmbed], ephemeral: true });
+  }
+
+  const filteredRoles = targetMember.roles.cache.filter((role) => role.id !== interaction.guild.id);
+  const listedRoles = filteredRoles.sort((a, b) => b.position - a.position).map((role) => role.toString());
+
+  const onlineStatus = beautifiedStatus[targetMember.presence?.status?.toUpperCase() ?? 'OFFLINE'];
+
+  let status: string;
+  if (targetMember.presence == null) {
+    status = 'Offline';
+  } else {
+    status = onlineStatus;
+  }
+
+  return interaction.reply({ embeds: [whoisEmbed(targetMember, status, listedRoles)] });
 });
